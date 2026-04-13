40c9a7149fddf22f52e4b74722151c717e5c5253
SujalHammad <sujalhammad@gmail.com>
2026-04-13 20:26:17 +0530

    fix bug of cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
-const express=require("express")
-const mongoose=require("mongoose")
-const cookieParser=require("cookie-parser")
-const dotenv=require("dotenv")
+const express = require("express")
+const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const dotenv = require("dotenv")
 dotenv.config()
 
-const {connectDb}=require("./db/db.js")
+const { connectDb } = require("./db/db.js")
 
 const { authRoute } = require("./routes/auth.route.js")
 const { organizerRoute } = require("./routes/organizer.routes.js")
@@ -13,24 +14,29 @@ const { sponsorRoute } = require("./routes/sponsor.route.js")
 
 require("./jobs/eventExpiry.job.js")
 
-const app=express()
+const app = express()
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({limit:"16kb",extended:true}))
+app.use(cors({
+    origin:`${process.env.CLIENT_ORIGIN}`,
+    credentials:true
+}))
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ limit: "16kb", extended: true }))
 app.use(cookieParser())
 
-app.use("/api/auth",authRoute)
-app.use("/api/organizer",organizerRoute)
-app.use("/api/admin",adminRoute)
-app.use("/api/sponsor",sponsorRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/organizer", organizerRoute)
+app.use("/api/admin", adminRoute)
+app.use("/api/sponsor", sponsorRoute)
 
 
 
-connectDb().then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
+connectDb().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`app is listening on the port ${process.env.PORT || 8000} `)
     })
-}).catch((err)=>{
+}).catch((err) => {
     console.error("connection error")
     process.exit(1)
 })
